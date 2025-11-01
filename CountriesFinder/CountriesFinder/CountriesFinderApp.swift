70e4256624bf6937ab1cfc59eec7c9c144b2180c
Amr Fawaz <amr.abdelhakim.fawaz+225@gmail.com>
2025-11-01 08:29:36 +0200

    Create new project

---
+++ CountriesFinder/CountriesFinder/CountriesFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesFinderApp.swift
+//  CountriesFinder
+//
+//  Created by Amr Fawaz on 01/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CountriesFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
